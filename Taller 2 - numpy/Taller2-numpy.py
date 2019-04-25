7b11cb08dcf6a3ecc27b7bef132caf056ef73932
David Aparicio <davidalejandro1223@hotmail.com>
2019-04-24 22:23:38 -0500

    eliminados print para mejor presentacion taller 2

--- Taller 2 - numpy/Taller2-numpy.py
+++ Taller 2 - numpy/Taller2-numpy.py
@@ -4,11 +4,6 @@ import copy
 mat = np.arange(729).reshape(9,9,9)
 print (mat)
 
-# p = x[1,0:4,0:4]
-# f = x[0,0:4,0:4]
-# p = f
-# print(p)
-
 idO = int(input('Ingrese el primer id \n'))
 idD = int(input('Ingrese el segundo id \n'))
 
@@ -23,14 +18,9 @@ limitesA=limites(idO)
 limitesB=limites(idD)
 
 tempA = mat[limitesA[0]*3:(limitesA[0]*3)+3, limitesA[1]*3:(limitesA[1]*3)+3, limitesA[2]*3:(limitesA[2]*3)+3]
-print(tempA)
 tempB = copy.deepcopy(mat[limitesB[0]*3:(limitesB[0]*3)+3, limitesB[1]*3:(limitesB[1]*3)+3, limitesB[2]*3:(limitesB[2]*3)+3])
-print(tempB)
 # Solo hace la primera linea
 mat[limitesB[0]*3:(limitesB[0]*3)+3, limitesB[1]*3:(limitesB[1]*3)+3, limitesB[2]*3:(limitesB[2]*3)+3] = tempA
 mat[limitesA[0]*3:(limitesA[0]*3)+3, limitesA[1]*3:(limitesA[1]*3)+3, limitesA[2]*3:(limitesA[2]*3)+3] = tempB     
-
-print(limitesA)
-print(limitesB)
 
 print(mat)
