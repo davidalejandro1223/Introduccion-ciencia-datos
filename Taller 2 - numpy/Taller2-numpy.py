2225c5a4f4b3c448fa2a6e1abbb69174a5035581
David Aparicio <davidalejandro1223@hotmail.com>
2019-04-24 22:21:13 -0500

    corregido el taller 2

--- Taller 2 - numpy/Taller2-numpy.py
+++ Taller 2 - numpy/Taller2-numpy.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 mat = np.arange(729).reshape(9,9,9)
 print (mat)
@@ -9,31 +10,27 @@ print (mat)
 # print(p)
 
 idO = int(input('Ingrese el primer id \n'))
-idD = int(input('Ingrese el primer id \n'))
+idD = int(input('Ingrese el segundo id \n'))
 
 
 def limites(id):
     x=id%3
     y=(id%9)//3
     z=id//9
+    return np.array([z,y,x])
 
-    limites = np.array([z,y,x])
-    return limites
+limitesA=limites(idO)
+limitesB=limites(idD)
 
-def mover(limitesA, limitesB):
-    tempA = mat[limitesA[0]*3:(limitesA[0]*3)+3, limitesA[1]*3:(limitesA[1]*3)+3, limitesA[2]*3:(limitesA[2]*3)+3]
-    print(tempA)
-    tempB = mat[limitesB[0]*3:(limitesB[0]*3)+3, limitesB[1]*3:(limitesB[1]*3)+3, limitesB[2]*3:(limitesB[2]*3)+3]
-    print(tempB)
-    # Solo hace la primera linea
-    mat[limitesB[0]*3:(limitesB[0]*3)+3, limitesB[1]*3:(limitesB[1]*3)+3, limitesB[2]*3:(limitesB[2]*3)+3] = tempA
-    mat[limitesA[0]*3:(limitesA[0]*3)+3, limitesA[1]*3:(limitesA[1]*3)+3, limitesA[2]*3:(limitesA[2]*3)+3] = tempB     
+tempA = mat[limitesA[0]*3:(limitesA[0]*3)+3, limitesA[1]*3:(limitesA[1]*3)+3, limitesA[2]*3:(limitesA[2]*3)+3]
+print(tempA)
+tempB = copy.deepcopy(mat[limitesB[0]*3:(limitesB[0]*3)+3, limitesB[1]*3:(limitesB[1]*3)+3, limitesB[2]*3:(limitesB[2]*3)+3])
+print(tempB)
+# Solo hace la primera linea
+mat[limitesB[0]*3:(limitesB[0]*3)+3, limitesB[1]*3:(limitesB[1]*3)+3, limitesB[2]*3:(limitesB[2]*3)+3] = tempA
+mat[limitesA[0]*3:(limitesA[0]*3)+3, limitesA[1]*3:(limitesA[1]*3)+3, limitesA[2]*3:(limitesA[2]*3)+3] = tempB     
 
-limA=limites(idO)
-limB=limites(idD)
-print(limA)
-print(limB)
-
-mover(limA, limB)
+print(limitesA)
+print(limitesB)
 
 print(mat)
