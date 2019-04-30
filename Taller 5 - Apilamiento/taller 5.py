151c030f5fb811b1b48c261014127cd9c69a7d24
David Aparicio <davidalejandro1223@hotmail.com>
2019-04-29 21:01:57 -0500

    terminado taller 5

--- Taller 5 - Apilamiento/taller 5.py
+++ Taller 5 - Apilamiento/taller 5.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib.pyplot import imread, imshow
 
-imagen = imread(fname="/home/david/Documentos/Introduccion a la ciencia de datos/Taller 5 - Apilamiento/imagen.png")
+imagen = imread(fname="imagen.png")
 print(imagen)
 imshow(imagen)
 
@@ -13,25 +13,34 @@ def limites(id):
     return limites
 
 def desordenar_matriz(matriz):
-    a = np.arange(16)
-    matrizAleatoria = np.random.choice(a, 16, replace=False).reshape((4,4))
+    matriz_aleatoria = np.random.choice(np.arange(16), 16, replace=False).reshape((4,4))
+    celdas_ver = np.zeros((55,275))
     for y in range(0,4,2):
-        imagenAleatoriaP = None
-        imagenAleatoriaS = None
+        celdas_hor_1 = np.zeros((55,55))
+        celdas_hor_2 = np.zeros((55,55))
         for x in range(0,4,2):
-            idP = limites(matrizAleatoria[y,x])
-            idS = limites(matrizAleatoria[y,x+1])
-            tempP = np.array(matriz[idP[0]*55:(idP[0]*55)+55, idP[1]*55:(idP[1]*55)+55])
-            tempS = np.array(matriz[idS[0]*55:(idS[0]*55)+55, idS[1]*55:(idS[1]*55)+55])
-            imagenAleatoriaP = np.array((tempP, tempS))
-        for x in range(0,4,2):
-            idP = limites(matrizAleatoria[y+1,x])
-            idS = limites(matrizAleatoria[y+1,x+1])
-            tempP = np.array(matriz[idP[0]*55:(idP[0]*55)+55, idP[1]*55:(idP[1]*55)+55])
-            tempS = np.array(matriz[idS[0]*55:(idS[0]*55)+55, idS[1]*55:(idS[1]*55)+55])
-            imagenAleatoriaS = np.hstack((tempP, tempS))
-        imagenfinal = np.vstack((imagenAleatoriaP, imagenAleatoriaS))
-    return imagenfinal
+            #se toman los primeros 4 id aleatorios de arriba-abajo y izq-derecha 
+            #de la matriz aleatoria para obtener los limites de cada bloque especifico
+            id_celda_1 = limites(matriz_aleatoria[y,x])
+            id_celda_2 = limites(matriz_aleatoria[y,x+1])
+            id_celda_3 = limites(matriz_aleatoria[y+1,x])
+            id_celda_4 = limites(matriz_aleatoria[y+1,x+1])
+            
+            #se extrae la informacion de cada bloque en especifico
+            celda_temp_1 = np.array(matriz[id_celda_1[0]*55:(id_celda_1[0]*55)+55, id_celda_1[1]*55:(id_celda_1[1]*55)+55])
+            celda_temp_2 = np.array(matriz[id_celda_2[0]*55:(id_celda_2[0]*55)+55, id_celda_2[1]*55:(id_celda_2[1]*55)+55])
+            celda_temp_3 = np.array(matriz[id_celda_3[0]*55:(id_celda_3[0]*55)+55, id_celda_3[1]*55:(id_celda_3[1]*55)+55])
+            celda_temp_4 = np.array(matriz[id_celda_4[0]*55:(id_celda_4[0]*55)+55, id_celda_4[1]*55:(id_celda_4[1]*55)+55])
+            
+            #se hace un apilamiento horizontal entre las celdas correspondientes a sus filas
+            celdas_hor_1 = np.hstack((celdas_hor_1,celda_temp_1, celda_temp_2))
+            celdas_hor_2 = np.hstack((celdas_hor_2,celda_temp_3, celda_temp_4))
 
+        #se hace un apilamiento vertical de cadas filas completas
+        celdas_ver = np.vstack((celdas_ver,celdas_hor_1, celdas_hor_2))
+    return celdas_ver
+
+#se elimina las primeras 55 filas y columnas puesto que fueron agregadas para poder
+#usar la funcion de apilamiento vertical y horizontal
 imagen_desordenada = desordenar_matriz(imagen)
-imshow(imagen_desordenada)
+imshow(imagen_desordenada[55:, 55:])
